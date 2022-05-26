24704e98b209d57fa97ec6a8f59248c72492f629
JMBenitez-dev <105891765+JMBenitez-dev@users.noreply.github.com>
2022-05-26 00:23:37 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 let v1: number[] = new Array(6);
 let v2: number[] = new Array(6);
 let posicion: number, indice: number;
-
+//tu solucion esta bien, como comentario aca los 2 for son identicos, esto es ideal para usar un funcion.
 for (indice = 0; indice < 6; indice++) {
   posicion = Number(
     prompt(
@@ -19,6 +19,10 @@ for (indice = 0; indice < 6; indice++) {
   );
   v2[indice] = posicion;
 }
+// lo ideal para hacer la suma es un for, algo por el estilo: 
+//for (i = 0; i < 6; i++) {
+//  Suma[i] = v1[i] + v2[i];
+//}
 let vSuma: number[] = [
   v1[0] + v2[0],
   v1[1] + v2[1],
@@ -27,6 +31,7 @@ let vSuma: number[] = [
   v1[4] + v2[4],
   v1[5] + v2[5]
 ];
+//
 console.log("Valores ingresados del primer arreglo: ", v1);
 console.log("Valores ingresados del segundo arreglo: ", v2);
 console.log("suma de los arreglo 1 + arreglo 2 : ", vSuma);
